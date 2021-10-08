3401bb4143f60a02c188938ccb0e4250c99added
Zena Wang <zena.Wang@kcl.ac.uk>
2021-10-08 23:28:28 +0800

    Enforce input validation on User model

--- microblogs/models.py
+++ microblogs/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+
 # Create your models here.
 
 class User(AbstractUser):
-    bio = models.TextField()
+    username = models.CharField(
+        max_length = 30,
+        unique = True,
+        validators = [RegexValidator(
+            regex = r'^@\w{3,}$',
+            message = 'Username must consist of @ followed by at least 3 alphanumericals'
+        )]
+    )
+
+    first_name = models.CharField(blank = False,max_length = 50,unique = False)
+    last_name = models.CharField(blank = False,max_length = 50,unique = False)
+    email = models.EmailField(unique = True,blank = False)
+    bio = models.CharField(max_length = 520, blank = True)
