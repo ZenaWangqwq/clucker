076084f33e06959c503671d318f8b73c0d790a2b
Zena Wang <zena.Wang@kcl.ac.uk>
2021-10-21 22:39:56 +0800

    prepare code for user story 02 -add test_log_in_form.py -add URL to log_in page add log_in in view dd log_in template -link home page to log_in page

---
+++ microblogs/tests/test-log-in-form.py
@@ -0,0 +1,7 @@
+"""Unit tests of the sign up form."""
+from django.test import TestCase
+
+
+class LogInFormTestCase(TestCase):
+    """Unit tests of sign up form."""
+    pass
--- microblogs/views.py
+++ microblogs/views.py
@@ -4,6 +4,9 @@ from .forms import SignUpForm
 
 
 # Create your views here.
+def log_in(request):
+    return render(request,'log_in.html')
+
 def feed(request):
     return render(request, 'feed.html')
 
