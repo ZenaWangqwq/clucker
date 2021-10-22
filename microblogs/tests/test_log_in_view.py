36b89410496a311b8f3ee997029cb3c2a3579503
Zena Wang <zena.Wang@kcl.ac.uk>
2021-10-22 14:54:37 +0800

    Test that valid log by inactive user fails

--- microblogs/tests/test_log_in_view.py
+++ microblogs/tests/test_log_in_view.py
@@ -11,14 +11,14 @@ class SignUpViewTestCase(TestCase, LogInTester):
 
     def setUp(self):
         self.url = reverse('log_in')
-        User.objects.create_user('@janedoe',
+        self.user = User.objects.create_user('@janedoe',
             first_name='Jane',
             last_name='Doe',
             email='janedoe@example.com',
             bio='Hello, I am Jane Doe.',
             password='Password123',
+            is_active=True,
             )
-
 
     def test_log_in_url(self):
         self.assertEqual(self.url, '/log_in/')
@@ -48,3 +48,15 @@ class SignUpViewTestCase(TestCase, LogInTester):
         response_url = reverse('feed')
         self.assertRedirects(response, response_url, status_code = 302, target_status_code = 200)
         self.assertTemplateUsed(response, 'feed.html')
+
+    def test_valid_log_in_by_inactive_user(self):
+        self.user.is_active=False
+        self.user.save()
+        form_input = {'username':'@janedoe','password':'Password123'}
+        response = self.client.post(self.url,form_input, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'log_in.html')
+        form = response.context['form']
+        self.assertTrue(isinstance(form, LogInForm))
+        self.assertFalse(form.is_bound)
+        self.assertFalse(self._is_logged_in())
