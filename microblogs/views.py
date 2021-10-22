af1f6521843839f5c9f23fd37956e0ffa18d0a7a
Zena Wang <zena.Wang@kcl.ac.uk>
2021-10-22 14:41:24 +0800

    Process succefful and unsucceful log ins

--- microblogs/views.py
+++ microblogs/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from .forms import SignUpForm
-from .forms import LogInForm
+from .forms import SignUpForm, LogInForm
+from django.contrib.auth import authenticate,login
 
 
 # Create your views here.
 def log_in(request):
+    if request.method == 'POST':
+        form = LogInForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('feed')
     form = LogInForm()
     return render(request,'log_in.html',{'form':form})
 
